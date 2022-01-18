bfcc067e9f28f09f88da8840bdffd683aa39b948
NotChiefOfficial <notchiefoficial@gmail.com>
2022-01-18 21:44:53 +0500

    made so that apache works with it

--- 404.php
+++ 404.php
@@ -1,23 +1,18 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <?php $pack = require "sources/test.php" ; ?>
-
     <link rel="stylesheet" href="css/main.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
     <title>PHP main</title>
   </head>
   <body class="d-flex flex-column min-vh-100">
     <?php 
-		$header = require "blocks/header.php";
-		$header.make_header(basename(__FILE__, '.php'));
-	?>
+			$header = require "blocks/header.php";
+			$header.make_header("error");
+		?>
 
     <div class="container mt-5">
-      <h3 class="mb-3">Hello World</h3>
-      <div class="d-flex flex-wrap">
-        <?php $pack.make_cards(5); ?>
-      </div>
+			<h3 class="mt-5 border-top">Sorry, this page seems not to exist. Error 404!</h3>
     </div>
 
     <?php require "blocks/footer.php"?>
